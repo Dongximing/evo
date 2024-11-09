06842cfeda1c9b987b43fdfa91ec838a22efd9c5
Dongximing <dongx34@myumanitoba.ca>
2024-11-09 13:21:28 -0500

    add exp

--- BBH/scripts/run_baseline_ga_cot2.sh
+++ BBH/scripts/run_baseline_ga_cot2.sh
@@ -8,7 +8,7 @@ POPSIZE=10
 llm=turbo
 initial=cot
 initial_mode=para_topk
-data_method=sampling_dynamic
+data_method=.....
 
 #for task in date_understanding dyck_languages  word_sorting  sports_understanding object_counting  formal_fallacies  causal_judgement  web_of_lies temporal_sequences disambiguation_qa tracking_shuffled_objects_three_objects penguins_in_a_table geometric_shapes snarks ruin_names tracking_shuffled_objects_seven_objects tracking_shuffled_objects_five_objects logical_deduction_three_objects hyperbaton logical_deduction_five_objects logical_deduction_seven_objects movie_recommendation salient_translation_error_detection reasoning_about_colored_objects multistep_arithmetic_two navigate boolean_expressions
 for task in metaphor_boolean
