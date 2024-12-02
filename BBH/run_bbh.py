7f3ad8eb5a85e6afb14880eeacf88ee35574ee54
Dongximing <dongx34@myumanitoba.ca>
2024-12-02 15:43:23 -0500

    add exp for nli

--- BBH/run_bbh.py
+++ BBH/run_bbh.py
@@ -226,6 +226,7 @@ def eval_task(task, task_prompt,cot_prompt,eval_data, client, model_index,logger
                 if not discrete:
                     find_index = find_token_index(list_top20_logprob, search_token)
                     if find_index == -1:
+                        logger.info(f"*************************index is -1 ******")
                         score = np.vstack((score, logit_matrix))
                     else:
                         if answers[index] == "ent":
