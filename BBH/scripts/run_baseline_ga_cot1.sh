5ed76b05d187d31b83b120c3e04b9cf324633e74
Dongximing <dongx34@myumanitoba.ca>
2024-12-02 11:20:59 -0500

    add exp for nli

--- BBH/scripts/run_baseline_ga_cot1.sh
+++ BBH/scripts/run_baseline_ga_cot1.sh
@@ -13,11 +13,11 @@ data_method=static_iteration
 #for task in date_understanding dyck_languages  word_sorting  sports_understanding object_counting  formal_fallacies  causal_judgement  web_of_lies temporal_sequences disambiguation_qa tracking_shuffled_objects_three_objects penguins_in_a_table geometric_shapes snarks ruin_names tracking_shuffled_objects_seven_objects tracking_shuffled_objects_five_objects logical_deduction_three_objects hyperbaton logical_deduction_five_objects logical_deduction_seven_objects movie_recommendation salient_translation_error_detection reasoning_about_colored_objects multistep_arithmetic_two navigate boolean_expressions
 for task in nli
 do
-for SIZE in 10
+for SIZE in 1
 do
 POPSIZE=$SIZE
 OUT_PATH=outputs/$task/$initial/ga/bd${BUDGET}_top${POPSIZE}_${initial_mode}_init/$llm/$data_method
-for seed in 111
+for seed in 115
 do
 mkdir -p $OUT_PATH/seed${seed}
 cache_path=cache/$task/seed$seed
